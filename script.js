da5448b7679845a5fe8ccc480c7e770a73a022c4
yashrajbharticybtekk <y.bharti@cybtekk.com>
2024-01-04 16:16:26 +0530

    working menu

--- script.js
+++ script.js
@@ -6,6 +6,17 @@ video.insertAdjacentElement("afterend", clone);
 let open = document.getElementById("open-menu");
 let menu = document.querySelector(".menu");
 open.addEventListener("click", () => {
+  open.animate(
+    [
+      { opacity: 1, rotate: "0" },
+      { opacity: 0, rotate: "75deg" },
+    ],
+    {
+      duration: 500,
+      iterations: 1,
+      easing: "cubic-bezier(0.17, 0.67, 0.83, 0.67)",
+    }
+  );
   menu.style.display = "grid";
   menu.animate([{ opacity: 0 }, { opacity: 1 }], {
     duration: 500,
@@ -17,6 +28,17 @@ open.addEventListener("click", () => {
 
 let close = document.querySelector(".close");
 close.addEventListener("click", () => {
+  close.animate(
+    [
+      { opacity: 1, rotate: "0" },
+      { opacity: 0, rotate: "75deg" },
+    ],
+    {
+      duration: 500,
+      iterations: 1,
+      easing: "cubic-bezier(0.17, 0.67, 0.83, 0.67)",
+    }
+  );
   menu.animate([{ opacity: 1 }, { opacity: 0 }], {
     duration: 500,
     iterations: 1,
